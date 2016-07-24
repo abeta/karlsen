136a5314ee8097169780e38f8b6a7f0ad1f35861
Adam Bouqdib <adam@abemedia.co.uk>
2016-07-24 15:13:20 +0000

    squash! update

--- _plugins/images.rb
+++ _plugins/images.rb
@@ -2,42 +2,42 @@ require "jekyll-assets"
 require 'base64'
 
 module Jekyll
-  
-  class ImageAbsolute < Jekyll::Assets::Liquid::Tag
-    def initialize(tag, args, tokens)
-      super("img", args, tokens)
-    end
-  
-    private
-    def build_html(args, sprockets, asset)
-      "file://" + asset.filename
-    end
-    
-  end
-  
+
+  # Inline image (base64)
   class ImageBase64 < Jekyll::Assets::Liquid::Tag
     def initialize(tag, args, tokens)
       super("img", args, tokens)
     end
-  
+
     private
     def build_html(args, sprockets, asset)
       "data:" + asset.content_type + ";base64, " + Base64.encode64(asset.source).delete("\n")
     end
-    
+
   end
-  
+
+  # For using proxies on images where only the path is required
   class ImagePath < Jekyll::Assets::Liquid::Tag
     def initialize(tag, args, tokens)
       super("img", args, tokens)
     end
-  
+
     private
     def build_html(args, sprockets, asset, path = get_path(sprockets, asset))
         path
     end
-  
+
   end
+
+  class ImageAbsolute < Jekyll::Assets::Liquid::Tag
+    def initialize(tag, args, tokens)
+      super("img", args, tokens)
+    end
+
+    private
+    def build_html(args, sprockets, asset)
+      "file://" + asset.filename
+    end
 
 end
 
@@ -45,8 +45,8 @@ end
 Liquid::Template.register_tag('image_path', Jekyll::ImagePath)
 Liquid::Template.register_tag('img_path', Jekyll::ImagePath)
 
-Liquid::Template.register_tag('image_base64', Jekyll::ImageAbsolute)
-Liquid::Template.register_tag('img_base64', Jekyll::ImageAbsolute)
+Liquid::Template.register_tag('image_base64', Jekyll::ImageBase64)
+Liquid::Template.register_tag('img_base64', Jekyll::ImageBase64)
 
-#Liquid::Template.register_tag('image_base64', Jekyll::ImageBase64)
-#Liquid::Template.register_tag('img_base64', Jekyll::ImageBase64)
+Liquid::Template.register_tag('image_absolute', Jekyll::ImageAbsolute)
+Liquid::Template.register_tag('img_absolute', Jekyll::ImageAbsolute)
